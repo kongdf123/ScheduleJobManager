0265236ecca6904ea3442ba080544adf044c104f
kent <kongdf123@hotmail.com>
2017-01-10 16:46:14 +0800

    Add project files.

---
+++ Admin/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult ScheduleJobList() {
+            return View();
+        }
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
