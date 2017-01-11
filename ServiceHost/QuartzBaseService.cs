ffa1aa1b412f66a09eaae37656cc0b6994a6c2b8
kent <kongdf123@hotmail.com>
2017-01-11 17:50:57 +0800

    Create ScheduleJob Services Host project

---
+++ ServiceHost/QuartzBaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ServiceHost
+{
+    public class QuartzBaseService : ServiceControl
+    {
+        public bool Start(HostControl hostControl) {
+            //TODO
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServiceHost/QuartzService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHost
+{
+    partial class QuartzService : ServiceBase
+    {
+
+        public QuartzService() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+            // TODO: Add code here to start your service.
+        }
+
+        protected override void OnStop() {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
