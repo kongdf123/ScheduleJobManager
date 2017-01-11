d7c92b9e7c34b2ca1a472757e40a6babf1c4b8d8
kent <kongdf123@hotmail.com>
2017-01-11 18:06:13 +0800

    Re-organize solutions

---
+++ JobServiceSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobServiceSite.Startup))]
+namespace JobServiceSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Service/Jobs/RecordsSyncSQLServerJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Jobs
+{
+    public class RecordsSyncSQLServerJob : IJob
+    {
+        public void Execute(IJobExecutionContext context) {
+            throw new NotImplementedException();
+        }
+    }
+}
