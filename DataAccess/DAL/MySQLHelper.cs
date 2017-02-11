88e726eee64f9173b26562c4d5a7f4b9568bb747
Kent <kongdf123@hotmail.com>
2017-02-11 23:26:16 +0800

    Add logic layer

---
+++ DataAccess/DAL/MySQLHelper.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Utility;
+
+namespace DataAccess.DAL
+{
+    public class MySQLHelper
+    {
+        static MySqlConnection GetConnection() {
+            MySqlConnection connection = new MySqlConnection();
+            connection.ConnectionString = ConfigurationManager.AppSettings["MySQLServerConnString"];
+            try {
+                connection.Open();
+            }
+            catch ( Exception ex ) {
+                connection.Close();
+                throw new CustomException("系统无法与数据库建立连结，请您与系统管理员联系。", ExceptionType.Error, "错误信息：" + ex.ToString());
+            }
+            return connection;
+        }
+
+        public static MySqlDataReader ExecuteReader(string commandText,MySqlParameter[] commandParms) {
+            MySqlConnection mySqlConnection = GetConnection();
+            MySqlCommand mySqlCommand = new MySqlCommand(commandText, mySqlConnection);
+            PrepareCommand(mySqlCommand, commandParms);
+            try {
+                MySqlDataReader dbReader = mySqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                return dbReader;
+            }
+            catch ( Exception ex) {
+                mySqlConnection.Close();
+                throw new CustomException("系统对数据库进行操作时发生错误，请您与系统管理员联系。",ExceptionType.Error,"执行语句："+commandText+"\n\t错误信息："+ex.ToString());
+            }
+        }
+
+        public static int ExecuteNonQuery(string commandText,MySqlParameter[] commandParms) {
+            MySqlConnection mySqlConnection = GetConnection();
+            MySqlCommand mySqlCommand = new MySqlCommand(commandText, mySqlConnection);
+            PrepareCommand(mySqlCommand, commandParms);
+            int rowCount = 0;
+            try {
+                rowCount = mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+            }
+            catch ( Exception ex) {
+                mySqlConnection.Close();
+                throw new CustomException("系统对数据库进行操作时发生错误，请您与系统管理员联系。", ExceptionType.Error, "执行语句：" + commandText + "\n\t错误信息：" + ex.ToString());
+            }
+            return rowCount;
+        }
+
+        static void PrepareCommand(MySqlCommand mySqlCommand,MySqlParameter[] commandParms) {
+            if ( commandParms!=null ) {
+                foreach ( var parameter in commandParms ) {
+                    if ( parameter.Value==null ) {
+                        parameter.Value = DBNull.Value;
+                    }
+                    mySqlCommand.Parameters.Add(parameter);
+                }
+            }
+        }
+    }
+}
---
+++ Utility/QuartzNetHelper.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Utility
+{
+    public class QuartzNetHelper
+    {
+        static IScheduler scheduler;
+
+        public static IScheduler GetScheduler() {
+            if ( scheduler == null ) {
+                NameValueCollection config = (NameValueCollection)ConfigurationManager.GetSection("quartz");
+                ISchedulerFactory factory = new StdSchedulerFactory(config);
+                scheduler = factory.GetScheduler();
+            }
+            return scheduler;
+        }
+
+        public void AddJob() {
+            //TODO
+        }
+
+
+    }
+}
