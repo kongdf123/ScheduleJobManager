6f13fa5fa6c263d54997cefb026cd897882b8ca6
kent <kongdf123@hotmail.com>
2017-02-16 18:17:28 +0800

    Buckup quartz db

--- DataAccess/Entity/quartz-net-mysql.sql
+++ DataAccess/Entity/quartz-net-mysql.sql
@@ -1,12 +1,16 @@
 ﻿CREATE DATABASE `quartz` /*!40100 DEFAULT CHARACTER SET utf8 */;
 CREATE TABLE `custom_db_config` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `ServerIP` varchar(30) NOT NULL,
+  `ServerAddress` varchar(30) NOT NULL,
   `DBName` varchar(45) NOT NULL,
   `UserName` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
   `UpdatedDate` datetime DEFAULT NULL,
   `CreatedDate` datetime DEFAULT NULL,
+  `EquipmentNum` varchar(25) NOT NULL,
+  `PageSize` int(5) DEFAULT NULL,
+  `MaxCapacity` int(10) DEFAULT NULL,
+  `StoredType` smallint(1) NOT NULL COMMENT '存储方式，1：按每页，2：按数据总量',
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='记录数据库连接配置信息';
 
@@ -27,7 +31,8 @@ CREATE TABLE `custom_job_details` (
   `Description` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `IntervalType` smallint(1) DEFAULT NULL COMMENT '执行间隔类型：\n1：天\n2：小时\n3：分钟',
   PRIMARY KEY (`JobId`,`JobName`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='to manage parameter records ,which belongs to control custom job actions in practice.';
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='to manage parameter records ,which belongs to control custom job actions in practice.';
+
 
 CREATE TABLE `qrtz_blob_triggers` (
   `SCHED_NAME` varchar(120) NOT NULL,
