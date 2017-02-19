2364e6ff70feaf2a4f2964c26f6383903900825a
Kent <kongdf123@hotmail.com>
2017-02-19 21:18:23 +0800

    Add test project

---
+++ TestSqlServerConnect/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSqlServerConnect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void TestBtn_Click(object sender, RoutedEventArgs e) {
+            string connectionStr = ConfigurationManager.AppSettings["SqlServer70"];
+            SqlConnection conn = new SqlConnection(connectionStr);
+            try {
+                conn.Open();
+                SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM syscolumns", conn);
+                MessageBox.Show(comm.ExecuteScalar().ToString());
+                conn.Close();
+            }
+            catch ( Exception ex ) {
+                conn.Close();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
