52a9c6cf6c39749b76a5277fd98f7427c120ed7d
kent <kongdf123@hotmail.com>
2017-02-23 17:11:05 +0800

    Add mongodb helper and usage demo

---
+++ MongoDBAppDemo/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBAppDemo
+{
+    public class BaseEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public BaseEntity()
+        {
+            this.Id = ObjectId.GenerateNewId().ToString();
+        }
+    }
+}
---
+++ MongoDBAppDemo/MongoHelper.cs
@@ -0,0 +1,75 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBAppDemo
+{
+    public class MongoHelper<T> where T : BaseEntity
+    {
+        IMongoDatabase database;
+        MongoUrl mongoUrl = new MongoUrl(ConfigurationManager.AppSettings["MongoDBConnString"]);
+
+        public MongoHelper()
+        {
+            MongoClientSettings settings = MongoClientSettings.FromUrl(mongoUrl);
+            //TODO
+            var client = new MongoClient(settings);
+            database = client.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        IMongoCollection<T> GetCollection(string collectionName)
+        {
+            return database.GetCollection<T>(collectionName);
+        }
+
+        public T Find(string collectionName, Expression<Func<T, bool>> filter)
+        {
+            var collection = GetCollection(collectionName);
+            return collection.Find(filter).FirstOrDefault();
+        }
+
+        public IList FindMany(string collectionName, Expression<Func<T, bool>> filter)
+        {
+            var collection = GetCollection(collectionName);
+            return collection.Find(filter).ToList();
+        }
+
+        public void Insert(string collectionName, T document)
+        {
+            var collection = GetCollection(collectionName);
+            collection.InsertOne(document);
+        }
+
+        public void Insert(string collectionName, IEnumerable<T> documents)
+        {
+            var collection = GetCollection(collectionName);
+            collection.InsertMany(documents);
+        }
+
+        public long Update(string collectionName, Expression<Func<T, bool>> filter, T document)
+        {
+            var collection = GetCollection(collectionName);
+            BsonDocument docUpdate = document.ToBsonDocument<T>();
+            docUpdate.Remove("_id");
+
+            UpdateDocument updateDifinition = new UpdateDocument("$set", docUpdate);
+            var result = collection.UpdateMany(filter, updateDifinition, new UpdateOptions { IsUpsert = false });
+
+            return result.ModifiedCount;
+        }
+
+        public long Delete(string collectionName, Expression<Func<T, bool>> filter)
+        {
+            var collection = GetCollection(collectionName);
+            DeleteResult result = collection.DeleteOne(filter);
+            return result.DeletedCount;
+        }
+    }
+}
