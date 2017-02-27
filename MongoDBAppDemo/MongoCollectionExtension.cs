8dcc18c4614a434466b59c464a7dfa60673fb8c2
kent <kongdf123@hotmail.com>
2017-02-27 15:30:41 +0800

    Implement mongodb access methods

---
+++ MongoDBAppDemo/MongoCollectionExtension.cs
@@ -0,0 +1,84 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDBAppDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBAppDemo
+{
+    public static class MongoCollectionExtension
+    {
+        /// <summary>  
+        /// ObjectId - Default Key
+        /// </summary>  
+        readonly static string OBJECTID_KEY = "_id";
+
+        public static bool Update<T>(this MongoCollection<T> collection, IMongoQuery query, IMongoUpdate update) where T : BaseEntity
+        {
+            WriteConcernResult result = collection.Update(query, update, UpdateFlags.Multi);            
+            return result.HasLastErrorMessage;
+        }
+
+        public static bool Insert<T>(this MongoCollection<T> collection, T document) where T : BaseEntity
+        {
+            BsonDocument bd = document.ToBsonDocument();
+            WriteConcernResult result = collection.Insert(bd);
+            return result.HasLastErrorMessage;
+        }
+
+        public static bool Insert<T>(this MongoCollection<T> collection, List<T> documents) where T : BaseEntity
+        {
+            List<BsonDocument> bsonList = new List<BsonDocument>();
+            documents.ForEach(t => bsonList.Add(t.ToBsonDocument()));
+            IEnumerable<WriteConcernResult> results = collection.InsertBatch(bsonList);
+            return results.ToList().Exists(x => x.HasLastErrorMessage);
+        }
+
+        public static T Find<T>(this MongoCollection<T> collection, IMongoQuery query) where T : BaseEntity
+        {
+            return collection.FindOne(query);
+        }
+
+        public static List<T> FindMany<T>(this MongoCollection<T> collection, IMongoQuery query) where T : BaseEntity
+        {
+            MongoCursor<T> mongoCursor = collection.Find(query);
+            return mongoCursor.ToList();
+        }
+
+        public static List<T> FindMany<T>(this MongoCollection<T> collection, IMongoQuery query, SortByDocument sortby, int pageIndex, int pageSize) where T : BaseEntity
+        {
+            if (query == null)
+            {
+                query = Query.Exists(OBJECTID_KEY);
+            }
+
+            if (sortby == null)
+            {
+                sortby = new SortByDocument(OBJECTID_KEY, -1);
+            }
+
+            MongoCursor<T> cursor = collection.FindAs<T>(query).SetSortOrder(sortby).SetSkip(Math.Max(1, pageIndex)).SetLimit(pageSize);
+            return cursor.ToList();
+        }
+
+        public static long Count<T>(this MongoCollection<T> collection, IMongoQuery query) where T : BaseEntity
+        {
+            if (query == null)
+            {
+                query = Query.Exists(OBJECTID_KEY);
+            }
+            return collection.Count(query);
+        }
+
+        /*
+             var post = _posts.Collection.Find(Query.EQ("_id", postId)).SetFields(Fields.Exclude("Date", "Title", "Url", "Summary", "Details", "Author", "TotalComments").Slice("Comments", -skip, limit)).Single();
+    return post.Comments.OrderByDescending(c => c.Date).ToList();
+         */
+
+    }
+}
--- MongoDBAppDemo/MongoHelper.cs
+++ MongoDBAppDemo/MongoHelper.cs
@@ -13,63 +13,16 @@ namespace MongoDBAppDemo
 {
     public class MongoHelper<T> where T : BaseEntity
     {
-        IMongoDatabase database;
-        MongoUrl mongoUrl = new MongoUrl(ConfigurationManager.AppSettings["MongoDBConnString"]);
+        public MongoCollection<T> Collection { get; private set; }
 
         public MongoHelper()
         {
+            MongoUrl mongoUrl = new MongoUrl(ConfigurationManager.ConnectionStrings["MongoDBConnString"].ConnectionString);
             MongoClientSettings settings = MongoClientSettings.FromUrl(mongoUrl);
             //TODO
             var client = new MongoClient(settings);
-            database = client.GetDatabase(mongoUrl.DatabaseName);
-        }
-
-        IMongoCollection<T> GetCollection(string collectionName)
-        {
-            return database.GetCollection<T>(collectionName);
-        }
-
-        public T Find(string collectionName, Expression<Func<T, bool>> filter)
-        {
-            var collection = GetCollection(collectionName);
-            return collection.Find(filter).FirstOrDefault();
-        }
-
-        public IFindFluent<T,T> FindMany(string collectionName, Expression<Func<T, bool>> filter)
-        {
-            var collection = GetCollection(collectionName);
-            return collection.Find(filter);
-        }
-
-        public void Insert(string collectionName, T document)
-        {
-            var collection = GetCollection(collectionName);
-            collection.InsertOne(document);
-        }
-
-        public void Insert(string collectionName, IEnumerable<T> documents)
-        {
-            var collection = GetCollection(collectionName);
-            collection.InsertMany(documents);
-        }
-
-        public long Update(string collectionName, T document, Expression<Func<T, bool>> filter)
-        {
-            var collection = GetCollection(collectionName);
-            BsonDocument docUpdate = document.ToBsonDocument<T>();
-            docUpdate.Remove("_id");
-
-            UpdateDocument updateDifinition = new UpdateDocument("$set", docUpdate);
-            var result = collection.UpdateMany(filter, updateDifinition, new UpdateOptions { IsUpsert = false });
-
-            return result.ModifiedCount;
-        }
-
-        public long Delete(string collectionName, Expression<Func<T, bool>> filter)
-        {
-            var collection = GetCollection(collectionName);
-            DeleteResult result = collection.DeleteOne(filter);
-            return result.DeletedCount;
+            var db = client.GetDatabase(mongoUrl.DatabaseName);
+            Collection = (MongoCollection<T>)(db.GetCollection<T>(typeof(T).Name.ToLower()));
         }
     }
 }
