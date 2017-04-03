61b94d3035aab2f8e5e3c1feb4db02f9dbd0bcef
Kent <kongdf123@hotmail.com>
2017-04-03 14:59:37 +0800

    Organize files

--- JobHostSite/Global.asax.cs
+++ JobHostSite/Global.asax.cs
@@ -15,50 +15,44 @@ namespace JobHostSite
 	// visit http://go.microsoft.com/?LinkId=9394801
 
 	public class WebApiApplication : System.Web.HttpApplication
-    {
-        IScheduler scheduler;
-        protected void Application_Start(object sender, EventArgs e)
-        {
-            AreaRegistration.RegisterAllAreas();
+	{
+		IScheduler scheduler;
+		protected void Application_Start(object sender, EventArgs e) {
+			AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            XmlConfigurator.Configure();
+			XmlConfigurator.Configure();
 
-            //初始化quart.net 
-            NameValueCollection config = (NameValueCollection)ConfigurationManager.GetSection("quartz");
-            scheduler = QuartzNetHelper.Initialize(config);
-            scheduler.Start();
+			//初始化quart.net 
+			NameValueCollection config = (NameValueCollection)ConfigurationManager.GetSection("quartz");
+			scheduler = QuartzNetHelper.Initialize(config);
+			scheduler.Start();
 
-            Log4NetHelper.WriteInfo("JobHostSite is started.");
-        }
+			Log4NetHelper.WriteInfo("JobHostSite is started.");
+		}
 
-        protected void Application_Error(object sender, EventArgs e)
-        {
-            Log4NetHelper.WriteExcepetion(Server.GetLastError());
-        }
+		protected void Application_Error(object sender, EventArgs e) {
+			Log4NetHelper.WriteExcepetion(Server.GetLastError());
+		}
 
-        protected void Application_End(object sender, EventArgs e)
-        {
-            if (scheduler != null)
-            {
-                scheduler.Shutdown(true);
-            }
-            Log4NetHelper.WriteInfo("网站关闭，系统资源回收。");
-
-            try
-            {
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:20170/");
-                HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
-                string desc = rsp.StatusDescription;
-                Log4NetHelper.WriteInfo(desc);
-            }
-            catch (Exception ex)
-            {
-                Log4NetHelper.WriteExcepetion(ex);
-            }
-        }
-    }
+		protected void Application_End(object sender, EventArgs e) {
+			if ( scheduler != null ) {
+				scheduler.Shutdown(true);
+			}
+			Log4NetHelper.WriteInfo("网站关闭，系统资源回收。");
+			// 防止IIS资源回收导致服务终止
+			try {
+				HttpWebRequest req = (HttpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["JobHostSite"]);
+				HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
+				string desc = rsp.StatusDescription;
+				Log4NetHelper.WriteInfo(desc);
+			}
+			catch ( Exception ex ) {
+				Log4NetHelper.WriteExcepetion(ex);
+			}
+		}
+	}
 }
